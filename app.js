e2b6b0a9a225560b339fa0caa4aa3df97ca9624b
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-08-01 15:14:17 +0600

    connection info dynamic

--- app.js
+++ app.js
@@ -5,5 +5,21 @@ const PORT=process.env.PORT || 3000
 const server = app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
+const io=require('socket.io')(server)
+
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', onConnected)
+let socketsConnected = new Set()
+
+
+function onConnected(socket) {
+    console.log('New client connected', socket.id)
+    socketsConnected.add(socket.id)
+    io.emit('clients-total', socketsConnected.size)
+    socket.on('disconnect',()=>{
+        socketsConnected.delete(socket.id)
+        io.emit("clients-total", socketsConnected.size);
+    })
+}
